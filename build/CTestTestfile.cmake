76d4968dff8cd5dd4446b235f9083c18d5815da5
tangyapeng <tangyapeng@haier.com>
2023-04-03 18:51:47 +0800

    ji

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tang/myproject/tang_test/librws_github/librws
-# Build directory: /home/tang/myproject/tang_test/librws_github/librws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
